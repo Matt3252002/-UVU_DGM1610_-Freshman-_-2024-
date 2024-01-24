f55fadf665ee2464460ab0208d0d0e81e123382a
Matt3252002 <156355077+Matt3252002@users.noreply.github.com>
2024-01-24 13:48:54 -0700

    C# Practice

---
+++ Practice.cs
@@ -0,0 +1,62 @@
+using System;
+
+namespace MyPractice
+{
+  class Program
+  {
+    static void Main(string[] args)
+    { 
+      int x = 12;
+      x += 4;
+      Console.WriteLine(x);
+//
+
+bool yay = true;
+bool nay = false;
+Console.WriteLine(bool);
+
+//
+
+string name = "Matt";
+Console.WriteLine(name);
+
+//
+
+int myNum = 10;
+Console.WriteLine(myNum);
+
+//
+
+int myNum;
+myNum = 5;
+Console.WriteLine(myNum);
+
+//
+
+int myNum = 12;
+myNum = 19; // myNum is now 19
+Console.WriteLine(myNum);
+
+//
+
+int myNum = 20000;
+Console.WriteLine(myNum);
+
+//
+
+long myNum = 380000000L;
+Console.WriteLine(myNum);
+
+//
+
+float myNum = 2.55;
+Console.WriteLine(myNum);
+
+//
+
+double myNum = 49.24;
+Console.WriteLine (myNum);
+
+     }
+  }
+}
