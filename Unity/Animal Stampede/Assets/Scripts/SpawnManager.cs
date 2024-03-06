43a070e9270ee813fbca9cab302ea1206369dcbf
MattFanco <Francomatt02@gmail.com>
2024-03-05 18:41:30 -0700

    Animal Stampede 3.0

--- Unity/Animal Stampede/Assets/Scripts/SpawnManager.cs
+++ Unity/Animal Stampede/Assets/Scripts/SpawnManager.cs
@@ -5,31 +5,29 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-    public PlayerController player;
-    private float spawnPosX = 20;
+    private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomAnimal", 2, 1.5f);
-        player = GameObject.Find("Player").GetComponent<PlayerController>();
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+            Instantiate(animalPrefabs[animalIndex], spawnPos , animalPrefabs[animalIndex].transform.rotation);
+        }
     }
 
-    void SpawnRandomAnimal()
-    {
-        if (player.gameOver)
-            return;
-        int animalIndex = Random.Range(0, animalPrefabs.Length);
-        Vector3 spawnPosition = new Vector3(Random.Range(-spawnPosX, spawnPosX), 0, spawnPosZ);
-        Instantiate(animalPrefabs[animalIndex], spawnPosition, animalPrefabs[animalIndex].transform.rotation);
-    }
+ 
 
 
 
