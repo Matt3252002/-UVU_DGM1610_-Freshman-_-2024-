7a7a9b636e0b36376e33b31ce8995725c4be266d
Matt3252002 <Francomatt02@gmail.com>
2024-04-24 22:45:42 -0600

    UDF MVP Game

--- Unity/UFO Defense Force/Assets/Scripts/ScoreManager.cs
+++ Unity/UFO Defense Force/Assets/Scripts/ScoreManager.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int score;
+    public TextMeshProUGUI scoreText;
+
+    
+    public void IncreaseScore(int amount)
     {
-        
+        score += amount;
+        UpdateScoreText();
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    public void DecreaseScore(int amount)
     {
-        
+        score -= amount;
+        UpdateScoreText();
+    }
+
+    public void UpdateScoreText()
+    {
+        scoreText.text = "Score: "+ score;
     }
 }
